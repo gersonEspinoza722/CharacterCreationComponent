013e0131c9945be396057a0fcc75e5368fe81ef2
gersonEspinoza722 <39819821+gersonEspinoza722@users.noreply.github.com>
2019-09-23 15:30:40 -0600

    Added media component and tools and characters done

--- src/BoardElement/Character/Concrete/Warrior.java
+++ src/BoardElement/Character/Concrete/Warrior.java
@@ -2,131 +2,31 @@ package BoardElement.Character.Concrete;
 
 import BoardElement.Character.AbstractCharacter;
 import BoardElement.Character.ICharacter;
-import BoardElement.IBoardElement;
-import BoardElement.Tools.ITool;
 import BoardElement.Tools.IToolListing;
-import Media.Concrete.Image;
-import Patterns.IBuilder;
-import Patterns.IPrototype;
-import Media.Concrete.ImageArray;
-import Media.IMediaListing;
 
-import java.io.File;
-import java.util.ArrayList;
-
-public class Warrior extends AbstractCharacter implements ICharacter, IPrototype<Warrior>, IBoardElement {
-
+public class Warrior extends AbstractCharacter implements ICharacter {
     private int stamina;
     private int speed;
 
-    public Warrior(float defaultLife, float decrementableLife, IToolListing tools, float level, float minPlayerLevelReq, float hitsPerUnit, int fields, int stamina, int speed) {
-        super(defaultLife, decrementableLife, tools, level, minPlayerLevelReq, hitsPerUnit, fields);
+    public Warrior(int stamina, int speed, float defaultLife, float decrementableLife, IToolListing tools, float level, float minPlayerLevelReq) {
+        super(defaultLife,decrementableLife,tools,level,minPlayerLevelReq);
         this.speed=speed;
         this.stamina=stamina;
-        super.media = new ImageArray();
-    }
-
-    public Warrior(float defaultLife, float decrementableLife, IToolListing tools, float level, float minPlayerLevelReq, float hitsPerUnit, int fields, IMediaListing media, int stamina, int speed) {
-        super(defaultLife, decrementableLife, tools, level, minPlayerLevelReq, hitsPerUnit, fields, media);
-        this.stamina = stamina;
-        this.speed = speed;
-    }
-
-    public int getStamina() {
-        return stamina;
-    }
-
-    public void setStamina(int stamina) {
-        this.stamina = stamina;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public IMediaListing getMedia() {
-        return super.media;
-    }
-
-    public void setMedia(ImageArray images) {
-        super.media = images;
     }
 
 
-    /**
-     * Decrements the current life points by the amount specified, if it reach 0 nothing happens
-     * @param amount: Amount of life points to decrement
-     */
     @Override
     public void decLife(int amount) {
-        if(decrementableLife - amount <= 0){
-            decrementableLife = 0;
-        }
-        else{
-            decrementableLife -= amount;
-        }
+
     }
 
-    /**
-     * Increments the current life points by the amount specified, if it reach the life cap nothing happens
-     * @param amount: Amount of life points to increment
-     */
     @Override
     public void incLife(int amount) {
-        if(decrementableLife + amount >= defaultLife){
-            decrementableLife = defaultLife;
-        }
-        else{
-            decrementableLife += amount;
-        }
+
     }
 
-    /**
-     * Sets the life cap points
-     * @param amount: Amount of life points to set
-     */
     @Override
     public void setDefaultLife(int amount) {
-        defaultLife = amount;
-    }
 
-    @Override
-    public IPrototype clone() {
-        Warrior clone = new Warrior(this.defaultLife, this.decrementableLife, this.tools, this.level, this.minPlayerLevelReq, this.hitsPerUnit, this.fields, this.stamina, this.speed);
-        return clone;
-    }
-
-    @Override
-    public IPrototype deepClone() {
-        IToolListing clonedTools = (IToolListing) this.tools.deepClone();
-        Warrior clone = new Warrior(this.defaultLife, this.decrementableLife, clonedTools, this.level, this.minPlayerLevelReq, this.hitsPerUnit, this.fields, this.media, this.stamina, this.speed);
-        return clone;
-    }
-
-    public static class WarriorBuilder implements IBuilder<AbstractCharacter> {
-        private int stamina, speed, fields;
-        private float defaultLife, decrementableLife, hitsPerUnit, level, minPlayerLevelReq;
-        IToolListing tools;
-        IMediaListing media;
-
-        @Override
-        public AbstractCharacter build() {
-            AbstractCharacter newWarrior = new Warrior(defaultLife, decrementableLife, tools, level, minPlayerLevelReq, hitsPerUnit, fields, media, stamina, speed);
-            return newWarrior;
-        }
-
-        public WarriorBuilder addTool(ITool tool){
-            this.tools.addTool(tool);
-            return this;
-        }
-
-        public WarriorBuilder addImage(String name, File file){
-            this.media.loadMedia(name, file);
-            return this;
-        }
     }
 }
--- src/BoardElement/Tools/Concrete/Weapon.java
+++ src/BoardElement/Tools/Concrete/Weapon.java
@@ -3,13 +3,8 @@ package BoardElement.Tools.Concrete;
 import BoardElement.IBoardElement;
 import BoardElement.Tools.AbstractTool;
 import BoardElement.Tools.ITool;
-import Patterns.IBuilder;
-import Patterns.IPrototype;
 
-public class Weapon extends AbstractTool implements ITool, IBoardElement, IPrototype<Weapon> {
-
-    public Weapon() {
-    }
+public class Weapon extends AbstractTool implements ITool, IBoardElement {
 
     public Weapon(float simpleUseDecrement, String name, int defaultLife, int decrementableLife, int reach, float level, float minCharacterLevelReq, float minPlayerLevelReq) {
         super(simpleUseDecrement,name, defaultLife, decrementableLife, reach, level, minCharacterLevelReq, minPlayerLevelReq);
@@ -33,6 +28,7 @@ public class Weapon extends AbstractTool implements ITool, IBoardElement, IProto
     @Override
     public void func(IBoardElement object) {//input can be character or tool to be affected by the weapon
 
+
     }
 
 
@@ -41,16 +37,4 @@ public class Weapon extends AbstractTool implements ITool, IBoardElement, IProto
     public void incLevel() {
 
     }
-
-    @Override
-    public IPrototype clone() {
-        return null;
-    }
-
-    @Override
-    public IPrototype deepClone() {
-        Weapon clonedWeapon = new Weapon(this.simpleUseDecrement, this.name, this.defaultLife, this.decrementableLife, this.reach, this.level, this.minCharacterLevelReq, this.minPlayerLevelReq);
-        return clonedWeapon;
-    }
-
 }
